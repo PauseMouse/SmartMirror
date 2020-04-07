e97cc26705618fd2dfd395199a3f146f5506d777
PauseMouse <61979418+PauseMouse@users.noreply.github.com>
2020-04-07 18:35:11 +0200

    Update install.sh
    Installation of apache2

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # This is an installer script. Created by the defelopers of Microsign Bedrijfssoftware as a fun project.
 
-echo Hello World!
+echo 'Hello World!'
+
+echo 'Check for updatable packages'
+sudo apt update
+
+echo 'Intall Apache2'
+sudo apt install apache2 -y
