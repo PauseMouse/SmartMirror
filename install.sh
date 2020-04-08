06f5bd45efb45c5ff139b5203df2bb26e228ad66
PauseMouse <61979418+PauseMouse@users.noreply.github.com>
2020-04-08 23:34:58 +0200

    Update install.sh
    - Check if SmartMirror is already installed. If so update Git.
    - Option to schedule auto updates. At 00:00.

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # This is an installer script. Created by the defelopers of Microsign Bedrijfssoftware as a fun project.
-
-echo 'Hello World!'
-
 echo 'Check for updatable packages'
 sudo apt update
 
@@ -22,21 +19,46 @@ sudo chmod -R 777 $(pwd)
 echo 'Remove defauld index.htm'
 sudo rm index.html
 
+if [[ -d "/var/www/html/SmartMirror/" ]]
+then
 if 
 	git clone --depth=1 https://github.com/PauseMouse/SmartMirror.git;
 then
 	echo 'Cloned to ' $(pwd);
 
-	echo 'Moving files from ' $(pwd)/SmartMirror/ ' to ' $(pwd)
-	mv -v $(pwd)/SmartMirror/* $(pwd)
-	rm -rf $(pwd)/SmartMirror/
+	echo 'Copy files from ' $(pwd)/SmartMirror/ ' to ' $(pwd)
+	cp -v $(pwd)/SmartMirror/* $(pwd)
 else
 	echo 'Unable to install'
 	exit;
 fi
+else
+	bash update.sh
+fi
 
 echo 'Copy new install.sh to /home/pi/'
 cp $(pwd)/install.sh /home/pi/
+
+echo 'Copy new update.sh to /home/pi/'
+cp $(pwd)/update.sh /home/pi/
+
+#schedule update task
+echo 'Just press [Enter] if you do not know what option is best.'
+read -p "Do you want to schedule auto updates? (Y/N default Y)? " choice
+choice="${choice:-Y}"
+if [[ $choice =~ ^[Yy]$ ]]; then
+	echo 'Sceduling Task'
+	cd /var/www/html/
+	chmod +X update.sh
+	(crontab -l 2>/dev/null || true; echo "0 0 * * * /var/www/html/update.sh") | crontab -
+	echo 'Starting cron'	
+	sudo service cron start
+	echo 'Update cron defaults'
+	sudo update-rc.d cron defaults
+else
+	echo 'To manualy update run bash update.sh'
+fi
+#schedule update task
 
 # load chromium after boot and open the smartmirror website in full screen mode
 mkdir -p /home/pi/.config/lxsession/LXDE-pi/
